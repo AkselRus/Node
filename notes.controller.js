6462c8c1f3afb92da2973642f65d33bcb677b24c
Алексей@HP <https://github.com/AkselRus>
2023-08-07 13:57:54 +0300

    init node

---
+++ notes.controller.js
@@ -0,0 +1,56 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+    const notes = await getNotes();
+    const note = {
+        title,
+        id: Date.now().toString(),
+    };
+
+    notes.push(note);
+
+    await saveNotes(notes);
+    console.log(chalk.bgGreen("Note was added!"));
+}
+
+async function getNotes() {
+    const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function saveNotes(notes) {
+    await fs.writeFile(notesPath, JSON.stringify(notes));
+}
+
+async function printNotes() {
+    const notes = await getNotes();
+
+    console.log(chalk.bgBlue("Here is the list of notes:"));
+    notes.forEach((note) => {
+        console.log(chalk.bgGray(note.id), chalk.blue(note.title));
+    });
+}
+
+async function removeNote(id) {
+    const notes = await getNotes();
+    const newNote = notes.filter(function (note) {
+        return note.id !== id;
+    });
+
+    if (notes.length > newNote.length) {
+        console.log(chalk.green.inverse("Note removed"));
+        saveNotes(newNote);
+    } else {
+        console.log(chalk.red.inverse("No note found"));
+    }
+}
+
+module.exports = {
+    addNote,
+    printNotes,
+    removeNote,
+};
